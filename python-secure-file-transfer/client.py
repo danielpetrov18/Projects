ec5da6996bb1340322ccc1ec7c30df49dfb54313
Daniel Petrov <daniel.petrov18@protonmail.com>
2024-08-23 22:17:08 +0300

    File gets transferred, however the data is corrupted. Needs to be fixed. Also add progress bar.

--- python-secure-file-transfer/client.py
+++ python-secure-file-transfer/client.py
@@ -1,4 +1,4 @@
-import os
+import sys
 import socket
 from Cryptodome.PublicKey import RSA
 from Cryptodome.Cipher import AES, PKCS1_OAEP
@@ -7,9 +7,10 @@ from Cryptodome.Random import get_random_bytes
 PORT = 4444
 BUFF_SIZE = 1024
 ENCODING = 'utf-8'
+EOT_MARKER = b'EOF'
 IP_ADDR = 'localhost'
 RSA_KEY_LENGTH = 3072
-PWD = b'I like turtles'  # TODO: load from a file (encrypted one)
+PWD = b'I like turtles'
 
 client_key_pair = RSA.generate(RSA_KEY_LENGTH)
 client_private_key = client_key_pair.export_key(
@@ -24,63 +25,44 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
     client_socket.connect((IP_ADDR, PORT))
     print('** CONNECTED TO SERVER **')
 
-    # Send public key
     client_socket.send(client_public_key)
     print('** SENT PUBLIC KEY **')
     
-    # Receive symmetric key and nonce
     encrypted_symmetric_key = client_socket.recv(BUFF_SIZE)
     nonce = client_socket.recv(BUFF_SIZE)
-    print('** RECEIVED SYMMETRIC KEY AND NONCE **')
+    print('** RECEIVED THE ENCRYPTED SYMMETRIC KEY AND THE NONCE **')
     
-    # Decrypt symmetric key
     client_private_key_obj = RSA.import_key(client_private_key, passphrase=PWD)
     cipher_rsa = PKCS1_OAEP.new(client_private_key_obj)
     decrypted_symmetric_key = cipher_rsa.decrypt(encrypted_symmetric_key)
+    print('** DECRYPTED THE SYMMETRIC KEY **')
     
-    # Prepare metadata
-    filename = 'file'
-    extension = 'mp3'
-    filesize = os.path.getsize(f'{filename}.{extension}')
-    metadata = f'{filename}:{extension}:{filesize}'.encode(ENCODING)
-    metadata_header = 'File metadata in the following format - filename:extension:filesize'.encode(ENCODING)
+    filename = sys.argv[1]
+    extension = sys.argv[2]
+    metadata = f'{filename}:{extension}'.encode(ENCODING)
     
-    # Encrypt and send metadata
     aes_cipher = AES.new(key=decrypted_symmetric_key, mode=AES.MODE_EAX, nonce=nonce)
-    aes_cipher.update(metadata_header)
     cipher_text, tag = aes_cipher.encrypt_and_digest(metadata)
+    
     client_socket.send(tag)
     client_socket.send(cipher_text)
     print('** SENT METADATA **')
     
-    # Read and encrypt the file in chunks
-    chunk_counter = 0
     with open(f'{filename}.{extension}', 'rb') as f:
-        while True:
-            chunk = f.read(BUFF_SIZE)
-            if not chunk:
-                break
-
-            # Generate a unique nonce for each chunk
-            chunk_nonce = nonce[:12] + chunk_counter.to_bytes(4, 'big')
-            aes_cipher = AES.new(key=decrypted_symmetric_key, mode=AES.MODE_EAX, nonce=chunk_nonce)
-            encrypted_chunk, chunk_tag = aes_cipher.encrypt_and_digest(chunk)
-            
-            # Send the encrypted chunk and its tag
-            client_socket.send(chunk_tag)
-            client_socket.send(encrypted_chunk)
-            print(f'Sent chunk {chunk_counter} with nonce {chunk_nonce.hex()}')
-            
-            # Wait for ACK from server
-            ack = client_socket.recv(BUFF_SIZE).decode(ENCODING)
-            if ack == "ACK":
-                print(f"Received ACK for chunk {chunk_counter}")
-            else:
-                print(f"Failed to receive ACK for chunk {chunk_counter}. Terminating.")
-                break
-            
-            chunk_counter += 1
-        
+        file_data = f.read()
+    
+    aes_cipher = AES.new(key=decrypted_symmetric_key, mode=AES.MODE_EAX, nonce=nonce)
+    encrypted_file_data, tag = aes_cipher.encrypt_and_digest(file_data)
+    
+    client_socket.send(encrypted_file_data)
+    client_socket.send(EOT_MARKER)
     print(f'** SENT FILE {filename}.{extension} **')
+    
+    print('** WAITING FOR ACK FROM SERVER **')
+    response = client_socket.recv(BUFF_SIZE).decode()
+    if response != 'ACK':
+        print('Something went wrong after sending the file to the server!')
+        client_socket.close()
+        exit(1)
 
-print('** CONNECTION CLOSED **')
+print('** CLIENT TERMINATED SUCCESSFULLY **')
--- python-secure-file-transfer/server.py
+++ python-secure-file-transfer/server.py
@@ -6,6 +6,7 @@ from Cryptodome.Random import get_random_bytes
 PORT = 4444
 BUFF_SIZE = 1024
 ENCODING = 'utf-8'
+EOT_MARKER = b'EOF'
 IP_ADDR = 'localhost'
 SYMMETRIC_KEY_NONCE = 16  # bytes
 SYMMETRIC_KEY_LENGTH = 32  # bytes
@@ -19,71 +20,58 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     with conn:
         print('** CONNECTION ESTABLISHED **')
         
-        # Receive and process the public key
         client_public_key = conn.recv(BUFF_SIZE)
         rsa_key = RSA.import_key(client_public_key)
+        print('** SERVER RECEIVED PUBLIC KEY **')
         
-        # Generate and send symmetric key and nonce
         symmetric_key = get_random_bytes(SYMMETRIC_KEY_LENGTH)
         nonce = get_random_bytes(SYMMETRIC_KEY_NONCE)
+        
         cipher_rsa = PKCS1_OAEP.new(rsa_key)
         encrypted_symmetric_key = cipher_rsa.encrypt(symmetric_key)
         
-        conn.sendall(encrypted_symmetric_key)
-        conn.sendall(nonce)
+        conn.send(encrypted_symmetric_key)
+        conn.send(nonce)
+        print('** SERVER SENT THE ENCRYPTED SYMMETRIC KEY AND THE NONCE **')
 
-        # Receive and decrypt metadata
-        tag = conn.recv(BUFF_SIZE)
-        client_cipher_text = conn.recv(BUFF_SIZE)
+        tag = conn.recv(16)
+        cipher_metadata = conn.recv(BUFF_SIZE)
         
-        metadata_header = 'File metadata in the following format - filename:extension:filesize'.encode(ENCODING)
         aes_cipher = AES.new(symmetric_key, AES.MODE_EAX, nonce=nonce)
-        aes_cipher.update(metadata_header)
-        
         try:
-            client_plain_text = aes_cipher.decrypt_and_verify(client_cipher_text, tag).decode(ENCODING)
+            metadata = aes_cipher.decrypt_and_verify(cipher_metadata, tag)
             print('** SERVER RECEIVED AND VERIFIED METADATA **')
         except (ValueError, KeyError) as e:
-            print('Decryption failed:', e)
+            print(f'Decryption failed: {e}')
             conn.close()
             exit(1)
         
-        # Extract metadata
-        metadata_contents = client_plain_text.split(':')
-        filename, extension, filesize = metadata_contents
+        metadata_fields = metadata.decode(ENCODING).split(':')
+        filename, extension = metadata_fields
         
-        # Receive and decrypt the file in chunks
         file_bytes = b''
-        chunk_counter = 0
-        
         while True:
-            chunk_tag = conn.recv(BUFF_SIZE)
             encrypted_file_chunk = conn.recv(BUFF_SIZE)
-            if not encrypted_file_chunk:
+            if not encrypted_file_chunk or encrypted_file_chunk.endswith(EOT_MARKER):
                 break
-            
-            # Generate the corresponding nonce for each chunk
-            chunk_nonce = nonce[:12] + chunk_counter.to_bytes(4, 'big')
-            aes_cipher = AES.new(symmetric_key, AES.MODE_EAX, nonce=chunk_nonce)
-            
-            try:
-                file_chunk = aes_cipher.decrypt_and_verify(encrypted_file_chunk, chunk_tag)
-                file_bytes += file_chunk
-                print(f'Received chunk {chunk_counter} with nonce {chunk_nonce.hex()}')
 
-                # Send ACK back to client
-                conn.sendall("ACK".encode(ENCODING))
-            except ValueError:
-                print("Chunk decryption failed. Integrity check failed.")
-                conn.sendall("NACK".encode(ENCODING))  # Negative acknowledgment
+            aes_cipher = AES.new(symmetric_key, AES.MODE_EAX, nonce=nonce)
+            try:
+                file_chunk = aes_cipher.decrypt(encrypted_file_chunk)
+                file_bytes += file_chunk
+                print('Receiving data ...')
+            except (ValueError, KeyError) as e:
+                print(f'Decryption failed for chunk: {e}')
                 conn.close()
                 exit(1)
-            
-            chunk_counter += 1
         
+        # Save the decrypted file
         with open(f'new_{filename}.{extension}', 'wb') as f:
             f.write(file_bytes)
-            
-        print(f'** SERVER RECEIVED new_{filename}.{extension} **')
+        
+        print(f'** SERVER RECEIVED AND SAVED new_{filename}.{extension} **')
+        
+        # Sending acknowledgment back to the client
+        conn.send('ACK'.encode(ENCODING))
 
-print('** CONNECTION CLOSED **')
+print('** SERVER TERMINATED SUCCESSFULLY **')
